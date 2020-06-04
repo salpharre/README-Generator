2e02978c291eeefe49f3077268fc4e01f8114e4f
Sandra <sandra.alpha2010@gmail.com>
2020-06-03 18:42:10 -0700

    psuedocoding structure in index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const axios = require ("axios");
+const axios = require("axios");
 
-inquirer
-    .prompt([
+function questions(){
+return inquirer.prompt([
         {
-                
+
         },
-    ]);
-}
+    ])
+};
+
+ function generateREADME(answers){
+     return ``
+ };
+
+questions()
+    .then(function({ username }){
+        //queryURL using username
+        //axios get call for profile photo and email
+        //writeFile
+    }).then(function(){
+        //console.log successful writing of file
+    }).catch(function(err){
+        //if (err) => err
+    })
